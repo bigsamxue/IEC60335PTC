e4f3d9c2db6fb095f7e6972d62312e71cccfc924
Omron <745350574@qq.com>
2023-09-27 09:18:29 +0800

    实时采集数据--已完成 Report完成--需修改强Copy（PlotView和DataGrid不可共用一个变量）

--- IEC60335Develop/Models/WTMeasureModel.cs
+++ IEC60335Develop/Models/WTMeasureModel.cs
@@ -1,18 +1,43 @@
-﻿using System;
+﻿using OxyPlot;
+using Prism.Mvvm;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace IEC60335Develop.Models {
-    public class WTMeasureModel {
-        //public double[] VoltageValue { get; set; }
-        //public double[] CurrentValue { get; set; }
-        //public double[] PowerValue { get; set; }
+    public class WTMeasureModel:BindableBase {
+
         public List<double> VoltageValue { get; set; }
         public List<double> CurrentValue { get; set; }
         public List<double> PowerValue { get; set; }
-        public string PowerMaxValue { get; set; }
+
+        private string _powerMaxValue;
+        public string PowerMaxValue {
+            get { return _powerMaxValue; }
+            set { SetProperty(ref _powerMaxValue, value); }
+        }
+
+        private string _voltageValueRT;
+        public string VoltageValueRT {
+            get { return _voltageValueRT; }
+            set { SetProperty(ref _voltageValueRT, value); }
+        }
+
+        private string _currentValueRT;
+        public string CurrentValueRT {
+            get { return _currentValueRT; }
+            set { SetProperty(ref _currentValueRT, value); }
+        }
+
+        private string _powerValueRT;
+        public string PowerValueRT {
+            get { return _powerValueRT; }
+            set { SetProperty(ref _powerValueRT, value); }
+        }
+
 
     }
 }
